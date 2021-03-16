436eda2fc22f867fee6b79bd23bf6671a1e1d274
Olumuyiwa Osiname <olumuyiwa.osiname@homeday.de>
2021-03-16 16:47:48 +0100

    uploader

---
+++ uploader.rb
@@ -0,0 +1,17 @@
+require 'cloudinary'
+# require "file"
+
+Cloudinary.config do |config|
+  config.cloud_name = 'uniodds'
+  config.api_key = '426593111487437'
+  config.api_secret = 'U07fL3MFp1JhvoDFJ6SSLd38Y8A'
+  config.enhance_image_tag = true
+  config.static_file_support = true
+end
+
+Dir.glob('/Users/olumuyiwaosiname/Desktop/gg/*png') do |file|
+  file_name = File.basename(file, ".*")
+  # "/Users/olumuyiwaosiname/234odds-ui/src/assets/images/icon-as-monaco.svg"
+  Cloudinary::Uploader.upload(file, quality: 10, public_id: file_name, fetch_format: :auto, invalidate: true, overwrite: true)
+end
+
