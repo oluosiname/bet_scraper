25e96077772002bbcbb268ad4d35be0f6a020a64
Olumuyiwa <oluosiname@gmail.com>
2021-03-25 16:15:13 +0100

    fix bug

--- surfer.js
+++ surfer.js
@@ -1,12 +1,8 @@
-const puppeteer = require("puppeteer"); // Development
+// const puppeteer = require("puppeteer"); // Development
 const chromium = require("chrome-aws-lambda");
 
 class Surfer {
   async launch() {
-    if (process.NODE_ENV === "development") {
-      return await puppeteer.launch({ headless: false });
-    }
-
     return await chromium.puppeteer.launch({
       args: chromium.args,
       defaultViewport: chromium.defaultViewport,
